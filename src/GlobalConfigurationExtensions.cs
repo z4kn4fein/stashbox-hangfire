54fceb8622a05559f669c88df36120eedbc7888b
Peter Csajtai <peter.csajtai@outlook.com>
2019-05-07 01:57:59 +0200

    Add ci and tests

--- src/GlobalConfigurationExtensions.cs
+++ src/GlobalConfigurationExtensions.cs
@@ -1,11 +1,22 @@
-﻿using Stashbox;
+﻿using System;
+using Stashbox;
 using Stashbox.Hangfire;
 using Stashbox.Utils;
 
 namespace Hangfire
 {
+    /// <summary>
+    /// Extension methods for the <see cref="IGlobalConfiguration"/>.
+    /// </summary>
     public static class GlobalConfigurationExtensions
     {
+        /// <summary>
+        /// Configures the given <see cref="IDependencyResolver"/> as the default <see cref="JobActivator"/> for resolving job dependencies.
+        /// </summary>
+        /// <param name="configuration">The global configuration.</param>
+        /// <param name="container">The dependency resolver.</param>
+        /// <returns>The <see cref="IGlobalConfiguration{StashboxJobActivator}"/> instance which uses the given Stashbox container to resolve job dependencies.</returns>
+        /// <exception cref="ArgumentNullException">When <paramref name="container"/> is null.</exception>
         public static IGlobalConfiguration<StashboxJobActivator> UseStashboxActivator(
             this IGlobalConfiguration configuration,
             IDependencyResolver container)
--- src/StashboxJobActivator.cs
+++ src/StashboxJobActivator.cs
@@ -4,21 +4,31 @@ using Hangfire.Server;
 
 namespace Stashbox.Hangfire
 {
+    /// <summary>
+    /// Used to resolve job dependencies with Stashbox.
+    /// </summary>
     public class StashboxJobActivator : JobActivator
     {
         private readonly IDependencyResolver container;
 
+        /// <summary>
+        /// Constructs a new <see cref="StashboxJobActivator"/>.
+        /// </summary>
+        /// <param name="container">The nested Stashbox dependency resolver used to resolve job dependencies.</param>
         public StashboxJobActivator(IDependencyResolver container)
         {
             this.container = container;
         }
 
+        /// <inheritdoc />
         public override object ActivateJob(Type jobType) =>
             this.container.Resolve(jobType);
 
+        /// <inheritdoc />
         public override JobActivatorScope BeginScope(JobActivatorContext context) =>
             new StashboxJobActivatorScope(this.container.BeginScope());
 
+        /// <inheritdoc />
         public override JobActivatorScope BeginScope(PerformContext context) =>
             new StashboxJobActivatorScope(this.container.BeginScope());
     }
